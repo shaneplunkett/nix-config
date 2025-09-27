54f995d830c4cba0a6ec6f00bd08d3fbaa86f634
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-09-27 15:58:50 +1000

    Neovim Refactor

--- home/shane/modules/common/nvim/lua/plugins/colorscheme.lua
+++ home/shane/modules/common/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  opts = {
+  require('catppuccin').setup {
     flavour = 'mocha',
     color_overrides = {
       mocha = {
