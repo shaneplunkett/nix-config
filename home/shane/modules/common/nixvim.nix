7826e4f5ce745be710dd270517d4eb3f2859c488
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-04 21:27:19 +1000

    nixvim

--- home/shane/modules/common/nixvim.nix
+++ home/shane/modules/common/nixvim.nix
@@ -4,6 +4,58 @@
   programs.nixvim = {
 
     enable = true;
+    
+    colorschemes.gruvbox.enable = true;
+
+    plugins = {
+    	lualine.enable = true;
+	tmux-navigatore.enable = true;
+	telescope.enable = true;
+	lsp.enable = true;
+	lazygit.enable = true;
+	which-key.enable = true;
+	noice.enable = true;
+	snacks.enable = true;
+	confirm.enable = true;
+	lazydev.enable = true;
+
+
+cmp = {
+  autoEnableSources = true;
+  settings.sources = [
+    { name = "nvim_lsp"; }
+    { name = "path"; }
+    { name = "buffer"; }
+  ];
+};
+
+treesitter = {
+enable = true;
+
+settings = {
+hightlight.enable = true;
+
+
+};
+
+
+};
+
+    };
+
+
+
+
+
+    lsp.servers.pyright = {
+    	enable = true;
+
+    };
+
+    lsp.servers.nixd = {
+    	enable = true;
+
+    };
 
   };
 
