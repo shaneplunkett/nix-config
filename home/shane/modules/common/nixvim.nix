28c5b15a7d000d1b80f7e551a4228ac6e74eef22
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-05 03:28:54 +1100

    nixvim

--- home/shane/modules/common/nixvim.nix
+++ home/shane/modules/common/nixvim.nix
@@ -707,6 +707,109 @@
         '';
         options.desc = "Neovim News";
       }
+      {
+        mode = "n";
+        key = "<leader>ot";
+        action.__raw = ''
+          function()
+            require('opencode').toggle()
+          end
+        '';
+        options.desc = "Toggle embedded";
+      }
+      {
+        mode = "n";
+        key = "<leader>oa";
+        action.__raw = ''
+          function()
+            require('opencode').ask '@cursor: '
+          end
+        '';
+        options.desc = "Ask about this";
+      }
+      {
+        mode = "v";
+        key = "<leader>oa";
+        action.__raw = ''
+          function()
+            require('opencode').ask '@selection: '
+          end
+        '';
+        options.desc = "Ask about selection";
+      }
+      {
+        mode = "n";
+        key = "<leader>o+";
+        action.__raw = ''
+          function()
+            require('opencode').prompt('@buffer', { append = true })
+          end
+        '';
+        options.desc = "Add buffer to prompt";
+      }
+      {
+        mode = "v";
+        key = "<leader>o+";
+        action.__raw = ''
+          function()
+            require('opencode').prompt('@selection', { append = true })
+          end
+        '';
+        options.desc = "Add selection to prompt";
+      }
+      {
+        mode = "n";
+        key = "<leader>oe";
+        action.__raw = ''
+          function()
+            require('opencode').prompt 'Explain @cursor and its context'
+          end
+        '';
+        options.desc = "Explain this code";
+      }
+      {
+        mode = "n";
+        key = "<leader>on";
+        action.__raw = ''
+          function()
+            require('opencode').command 'session_new'
+          end
+        '';
+        options.desc = "New session";
+      }
+      {
+        mode = "n";
+        key = "<S-C-u>";
+        action.__raw = ''
+          function()
+            require('opencode').command 'messages_half_page_up'
+          end
+        '';
+        options.desc = "Messages half page up";
+      }
+      {
+        mode = "n";
+        key = "<S-C-d>";
+        action.__raw = ''
+          function()
+            require('opencode').command 'messages_half_page_down'
+          end
+        '';
+        options.desc = "Messages half page down";
+      }
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>os";
+        action.__raw = ''
+          function()
+            require('opencode').select()
+          end
+        '';
+        options.desc = "Select prompt";
+      }
       # more keymaps here...
     ];
 
@@ -832,12 +935,14 @@
       };
       cmp = {
         autoEnableSources = true;
-        settings.sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
-
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+        };
       };
 
       conform-nvim = {
