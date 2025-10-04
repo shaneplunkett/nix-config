a59b213d753abc7b0a9978dd0c6e3120307fb90d
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-05 00:35:14 +1000

    nixvim

--- home/shane/modules/common/nixvim.nix
+++ home/shane/modules/common/nixvim.nix
@@ -9,21 +9,29 @@
     colorschemes.catppuccin.enable = true;
 
     globals = {
-    have_nerd_font = true;
-    mapleader = " ";
-    maplocalleader = " ";
+      have_nerd_font = true;
+      mapleader = " ";
+      maplocalleader = " ";
 
     };
 
-  keymaps = [
-    {
-      mode = "i";
-      key = "jj";
-      action = "<Esc>";
-      options.desc = "Exit insert mode";
-    }
-    # more keymaps here...
-  ];
+    keymaps = [
+      {
+        mode = "i";
+        key = "jj";
+        action = "<Esc>";
+        options.desc = "Exit insert mode";
+      }
+      {
+        mode = "n";
+        key = "<leader>gg";
+        action = "<cmd>LazyGit<CR>";
+        options = {
+          desc = "LazyGit (root dir)";
+        };
+      }
+      # more keymaps here...
+    ];
 
     opts = {
       number = true;
@@ -58,7 +66,6 @@
       cursorline = true;
       scrolloff = 999;
 
-
     };
     plugins = {
       web-devicons.enable = true;
@@ -70,62 +77,152 @@
       lazygit.enable = true;
       which-key.enable = true;
       noice.enable = true;
-      snacks.enable = true;
-      conform-nvim.enable = true;
+      opencode = {
+        enable = true;
+        settings = {
+          input.enabled = true;
+
+        };
+
+      };
+      snacks = {
+        enable = true;
+        settings = {
+          bigfile.enable = true;
+          animate.enable = false;
+          indent.enable = true;
+          input.enable = true;
+          scope.enable = true;
+          scroll.enable = true;
+          statuscolumn.enable = true;
+          words.enable = true;
+
+          notifier = {
+            enable = true;
+            timeout = 3000;
+            top_down = false;
+            border = "rounded";
+          };
+          styles = {
+            notification = {
+              wo = {
+                wrap = true;
+              };
+
+            };
+
+          };
+        };
+      };
+      blink-cmp = {
+        enable = true;
+        setupLspCapabilities = true;
+
+      };
+
+      conform-nvim = {
+        enable = true;
+        settings = {
+          formatters_by_ft = {
+            "_" = [ "time_whitespace" ];
+            css = [ "biome" ];
+            html = [ "biome" ];
+            svg = [ "biome" ];
+            fish = [ "fish_indent" ];
+            javascript = [ "prettier" ];
+            javascriptreact = [ "prettier" ];
+            json = [ "prettier" ];
+            jsonc = [ "prettier" ];
+            lua = [ "stylua" ];
+            nix = [ "nixfmt" ];
+            mdx = [ "prettier" ];
+            markdown = [ "prettier" ];
+            python = [ "ruff" ];
+            sh = [ "shfmt" ];
+            svelte = [ "prettier" ];
+            typescript = [ "prettier" ];
+            rust = [ "rustfmt" ];
+            go = [ "gofmt" ];
+            astro = [ "prettier" ];
+            terraform = [ "terraform_fmt" ];
+            tf = [ "terraform_fmt" ];
+            sql = [ "sqlfluff" ];
+            pgsql = [ "sqlfluff" ];
+          };
+          format_on_save = ''
+            function(bufnr)
+            local ignore_filetypes = { "helm" }
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+            return
+            end
+
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+            end
+
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match("/node_modules/") then
+            return
+            end
+            return { timeout_ms = 1000, lsp_fallback = true }
+            end
+          '';
+        };
+
+      };
       lazydev.enable = true;
       todo-comments.enable = true;
       neo-tree.enable = true;
       treesitter-context.enable = false;
       treesitter-textobjects = {
-                enable = true;
-                select = {
-                    enable = true;
-                    lookahead = true;
-                };
-            };
+        enable = true;
+        select = {
+          enable = true;
+          lookahead = true;
+        };
+      };
       treesitter = {
-      enable = true;
-      settings = {
-      indent = {
-      enable = true;
+        enable = true;
+        settings = {
+          indent = {
+            enable = true;
+
+          };
+          nixvimInjections = true;
+          grammerPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+        };
 
       };
-      nixvimInjections = true;
-      grammerPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+      lsp = {
+        enable = true;
+        inlayHints = true;
+        servers = {
 
-      };
-
-      };
-            lsp-format = {
-                enable = true;
+          nixd = {
+            enable = true;
+            settings = {
+              formatting.command = [ "nixpkgs-fmt" ];
+              nixpkgs.expr = "import <nixpkgs> {}";
             };
-            lsp = {
-                enable = true;
-                inlayHints = true;
-                servers = {
+          };
 
+          gopls = {
+            enable = true;
+            autostart = true;
+          };
+          pyright = {
+            enable = true;
+          };
+          lua_ls = {
+            enable = true;
+            settings.telemetry.enable = false;
+          };
 
-                    nixd = {
-                        enable = true;
-                    };
+        };
 
-                    gopls = {
-                        enable = true;
-                        autostart = true;
-                    };
-                    pyright = {
-                        enable = true;
-                    };
-                    lua_ls = {
-                        enable = true;
-                        settings.telemetry.enable = false;
-                    };
-
-
-                };
-
-
-            };
+      };
     };
   };
 }
--- home/shane/modules/common/packages.nix
+++ home/shane/modules/common/packages.nix
@@ -12,5 +12,32 @@
     go
     lazydocker
     opencode
+
+    nixfmt
+    black
+    pyright
+    gopls
+    stylua
+    prettier
+
+    nixd
+    nil
+    lua-language-server
+    typescript-language-server
+    bash-language-server
+    astro-language-server
+    docker-language-server
+    gopls
+    terraform-ls
+    vscode-langservers-extracted
+    markdown-oxide
+    ruff
+    yaml-language-server
+    omnisharp-roslyn
+    tailwindcss-language-server
+    phpactor
+    rubyPackages_3_3.ruby-lsp
+    sqls
+    glsl_analyzer
   ];
 }
