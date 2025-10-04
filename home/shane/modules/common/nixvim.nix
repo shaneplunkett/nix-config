510b9e3fbc31f8af0a384e4a91f996bda8ea70e9
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-04 21:47:37 +1000

    nixvim

--- home/shane/modules/common/nixvim.nix
+++ home/shane/modules/common/nixvim.nix
@@ -4,20 +4,55 @@
   programs.nixvim = {
 
     enable = true;
-    
-    colorschemes.gruvbox.enable = true;
+   globals.mapleader = " "; 
+    colorschemes.catppuccin.enable = true;
 
+    globalOpts = {
+    number = true;
+    relative = true;
+    completeopt = [
+    "menuone"
+    "noselect"
+    "noinsert"
+    ];
+
+    signcolumn  = "yes:3";
+
+    mouse = "a";
+    
+     clipboard = {
+      providers.wl-copy.enable = true;
+      register = "wl-copy";
+    };
+
+    scrolloff = 5;
+
+
+    };
+autoCmd = [
+    {
+      event = [ "VimEnter" ];
+      callback = {
+        __raw = "function() if vim.fn.argv(0) == '' then require('telescope.builtin').find_files() end end";
+      };
+    }
+    {
+      event = [ "VimEnter" ];
+      command = "set relativenumber";
+    }
+  ];
     plugins = {
     	lualine.enable = true;
-	tmux-navigatore.enable = true;
+	tmux-navigator.enable = true;
 	telescope.enable = true;
 	lsp.enable = true;
 	lazygit.enable = true;
 	which-key.enable = true;
 	noice.enable = true;
 	snacks.enable = true;
-	confirm.enable = true;
+	conform-nvim.enable = true;
 	lazydev.enable = true;
+	web-devicons.enable = true;
 
 
 cmp = {
