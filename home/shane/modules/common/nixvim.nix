c15c492f9b1e7a2fe56494cc5f1679224914d97e
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-04 22:11:39 +1000

    nixvim

--- home/shane/modules/common/nixvim.nix
+++ home/shane/modules/common/nixvim.nix
@@ -1,97 +1,50 @@
 { pkgs, ... }:
 {
-
   programs.nixvim = {
-
     enable = true;
-   globals.mapleader = " "; 
+    globals.mapleader = " ";
     colorschemes.catppuccin.enable = true;
-
-    globalOpts = {
-    number = true;
-    relativenumber = true;
-    completeopt = [
-    "menuone"
-    "noselect"
-    "noinsert"
-    ];
-
-    signcolumn  = "yes:3";
-
-    mouse = "a";
     
-     clipboard = {
-      providers.wl-copy.enable = true;
-      register = "wl-copy";
+    opts = {
+      number = true;
+      relativenumber = true;
+      completeopt = [
+        "menuone"
+        "noselect"
+        "noinsert"
+      ];
+      signcolumn = "yes:3";
+      mouse = "a";
+      clipboard = "unnamedplus";
+      scrolloff = 5;
     };
-
-    scrolloff = 5;
-
-
-    };
-autoCmd = [
-    {
-      event = [ "VimEnter" ];
-      callback = {
-        __raw = "function() if vim.fn.argv(0) == '' then require('telescope.builtin').find_files() end end";
-      };
-    }
-    {
-      event = [ "VimEnter" ];
-      command = "set relativenumber";
-    }
-  ];
+    
+    autoCmd = [
+      {
+        event = [ "VimEnter" ];
+        callback = {
+          __raw = "function() if vim.fn.argv(0) == '' then require('telescope.builtin').find_files() end end";
+        };
+      }
+      {
+        event = [ "VimEnter" ];
+        command = "set relativenumber";
+      }
+    ];
+    
     plugins = {
-	web-devicons.enable = true;
-    	lualine.enable = true;
-#	tmux-navigator.enable = true;
-	telescope.enable = true;
-	lsp.enable = true;
-	#lazygit.enable = true;
-	#which-key.enable = true;
-#	noice.enable = true;
-#	snacks.enable = true;
-#	conform-nvim.enable = true;
-#	lazydev.enable = true;
-
-
-cmp = {
-  autoEnableSources = true;
-  settings.sources = [
-    { name = "nvim_lsp"; }
-    { name = "path"; }
-    { name = "buffer"; }
-  ];
-};
-
-treesitter = {
-enable = true;
-
-settings = {
-hightlight.enable = true;
-
-
-};
-
-
-};
-
+      # web-devicons.enable = true;
+      # lualine.enable = true;
+      # tmux-navigator.enable = true;
+      # telescope.enable = true;
+      # lazygit.enable = true;
+      # which-key.enable = true;
+      # noice.enable = true;
+      # snacks.enable = true;
+      # conform-nvim.enable = true;
+      # lazydev.enable = true;
+      
+      
     };
-
-
-
-
-
-    lsp.servers.pyright = {
-    	enable = true;
-
-    };
-
-    lsp.servers.nixd = {
-    	enable = true;
-
-    };
-
   };
-
 }
