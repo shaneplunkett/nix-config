7a732c347aecbee23de1dfcf409424bd95436120
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-07 11:16:36 +1100

    Refactor

--- home/shane/homemac.nix
+++ home/shane/homemac.nix
@@ -6,11 +6,12 @@
     ./modules/common/cava.nix
     ./modules/common/direnv.nix
     ./modules/common/fish.nix
-    ./modules/common/neovim.nix
     ./modules/common/starship.nix
     ./modules/common/packages.nix
     ./modules/common/tmux.nix
     ./modules/common/git.nix
+    ./modules/common/nixvim/default.nix
+    ./modules/common/stylix.nix
 
     ./modules/macos/ghostty.nix
     ./modules/macos/packages.nix
