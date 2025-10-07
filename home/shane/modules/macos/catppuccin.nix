f017eae7068bc27344e4b5835b9801b9efe1093c
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-07 18:55:41 +1100

    theming

--- home/shane/modules/macos/catppuccin.nix
+++ home/shane/modules/macos/catppuccin.nix
@@ -6,17 +6,7 @@
     fish.enable = true;
     ghostty.enable = true;
     starship.enable = true;
-    gtk.icon = {
-      enable = true;
-      accent = "mauve";
-      flavor = "mocha";
-    };
-    hyprland = {
-      enable = true;
-      accent = "pink";
-      flavor = "mocha";
-
-    };
+    lsd.enable = true;
   };
 
 }
