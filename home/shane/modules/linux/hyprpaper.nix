1e7537a26047b77e2032d080d23525c2df405030
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-09 23:51:43 +1100

    theming

--- home/shane/modules/linux/hyprpaper.nix
+++ home/shane/modules/linux/hyprpaper.nix
@@ -3,8 +3,8 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "~/nix-config/home/shane/wallpapers/wallpaper.png" ];
-      wallpaper = [ " , ~/nix-config/home/shane/wallpapers/wallpaper.png" ];
+      preload = [ "~/nix-config/home/shane/wallpapers/11356566-1513361830.jpg" ];
+      wallpaper = [ " , ~/nix-config/home/shane/wallpapers/11356566-1513361830.jpg" ];
     };
   };
 }
