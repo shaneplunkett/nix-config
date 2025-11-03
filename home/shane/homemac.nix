0bca9274d5dba53ce3ad22749812aa5495da2407
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-11-03 14:22:01 +1100

    refactor

--- home/shane/homemac.nix
+++ home/shane/homemac.nix
@@ -3,8 +3,7 @@
 
   imports = [
     ./modules/common
-
-    ./modules/macos/ghostty.nix
+    ./modules/macos
   ];
 
   home.username = "shane";
---
+++ home/shane/modules/macos/default.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+
+  imports = [
+    ./ghostty.nix
+
+  ];
+
+}
