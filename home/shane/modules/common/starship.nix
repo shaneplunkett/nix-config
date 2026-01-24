9631541c582e3ea6c34671c01255cb08fd9f5afd
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-01-25 02:39:38 +1100

    update

--- home/shane/modules/common/starship.nix
+++ home/shane/modules/common/starship.nix
@@ -3,5 +3,11 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      gcloud = {
+        format = "on [$symbol$account(@$domain) ($project)]($style) ";
+        detect_env_vars = [ "GOOGLE" ];
+      };
+    };
   };
 }
