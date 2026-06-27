8b13f2adea6f4b656fd344e422509179c29a69bc
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-06-27 15:46:44 +1000

    Allow passwordless nh NixOS switches

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -16,11 +16,38 @@ _: {
       users = [ "shane" ];
       commands = [
         {
+          command = "/nix/store/*-nixos-system-*/bin/switch-to-configuration boot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/nix/store/*-nixos-system-*/bin/switch-to-configuration test";
+          options = [ "NOPASSWD" ];
+        }
+        {
           command = "/nix/store/*-nixos-system-*/bin/switch-to-configuration switch";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/env * /nix/store/*-nixos-system-*/bin/switch-to-configuration test";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/env * /nix/store/*-nixos-system-*/bin/switch-to-configuration switch";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/env * /nix/store/*-nixos-system-*/bin/switch-to-configuration boot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/env * nix build --no-link --profile /nix/var/nix/profiles/system /nix/store/*-nixos-system-*";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/env * ln -sfn /nix/var/nix/profiles/system-*-link /nix/var/nix/profiles/system";
           options = [ "NOPASSWD" ];
         }
       ];
     }
   ];
-
 }
